862d8dcef5d7fec0db5c3385459991d37fff8b73
Куликова Кристина <124985991+proyll@users.noreply.github.com>
2023-03-31 11:38:57 +0300

    Add files via upload

---
+++ Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace DataAccess.Interfaces
+{
+    public interface IUserRepository : IRepositoryBase<Userss>
+    {
+    }
+}
---
+++ Domain/Models/Booking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Models
+{
+    public partial class Booking
+    {
+        public int BookingId { get; set; }
+        public int CartId { get; set; }
+        public int? Price { get; set; }
+        public string StatusB { get; set; } = null!;
+        public string Delivery { get; set; } = null!;
+        public string AddressB { get; set; } = null!;
+
+        public virtual Cart Cart { get; set; } = null!;
+    }
+}
