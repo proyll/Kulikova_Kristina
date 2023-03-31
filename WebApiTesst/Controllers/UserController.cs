7123f0a6a9622b759108859467846276957a8123
Куликова Кристина <124985991+proyll@users.noreply.github.com>
2023-03-31 11:40:21 +0300

    Add files via upload

--- WebApiTesst/Controllers/UserController.cs
+++ WebApiTesst/Controllers/UserController.cs
@@ -1,5 +1,5 @@
 ﻿using BusinessLogic.Interfaces;
-using DataAccess.Models;
+using Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApiTesst.Controllers
--- WebApiTesst/Program.cs
+++ WebApiTesst/Program.cs
@@ -1,6 +1,5 @@
 using System.Reflection;
-using DataAccess.Models;
-using DataAccess.Wrapper;
+using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
@@ -14,15 +13,40 @@ namespace WebApiTesst
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+          
+
+            // Add services to the container.
             builder.Services.AddDbContext<OdezdaContext>(
-                optionsAction: options => options.UseSqlServer(
-                    connectionString: "LAPTOP - OUBQPCO5; Database = Odezda; Integrated Security = True;"));
+               optionsAction: options => options.UseSqlServer(
+                   connectionString: "Server= lab116-p;Database= api_market;User Id= sa;Password= 12345;"));
 
             builder.Services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
             builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
         }
     }
 }
 
 
-//Server = LAPTOP - OUBQPCO5; Database = Odezda; Integrated Security = True;
+//Server= lab116-p;Database= Odezhda;User Id= sa;Password= 12345;
