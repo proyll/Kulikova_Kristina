ac95b59d59b0366b1a9dfd431ffa5f0074bbe14f
Куликова Кристина <124985991+proyll@users.noreply.github.com>
2023-04-17 11:32:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,73 @@
+using System.Reflection;
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using BusinessLogic.Interfaces;
+using BusinessLogic.Services;
+
+namespace WebApiTesst
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            // Add services to the container.
+            builder.Services.AddDbContext<OdezdaContext>(
+               optionsAction: options => options.UseSqlServer(
+                   connectionString: "Server= lab116-p;Database= api_market;User Id= sa;Password= 12345;"));
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Интернет-магазин API",
+                    Description = "Краткое описание вашего API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Пример контакта",
+                        Url = new Uri("https://example.com/contact")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Пример лицензии",
+                        Url = new Uri("https://example.com/license")
+                    }
+                });
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            });
+
+            builder.Services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+           
+
+        }
+    }
+}
+
+
+//Server= lab116-p;Database= Odezhda;User Id= sa;Password= 12345;
